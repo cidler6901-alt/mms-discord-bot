5381c46e54a4f1e0ac291724a6b0dd086610ffa8
Carson <carsonbro640@gmail.com>
2026-02-13 17:39:17 -0700

    Delete models directory

--- models/Homework.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model("Homework", new mongoose.Schema({
-  title: String,
-  due: String,
-  createdAt: { type: Date, default: Date.now }
-}));
